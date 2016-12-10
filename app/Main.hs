1ba44b211855c70af44463adf99303fc42be7121
Pierre Radermecker <pradermecker@cirb.brussels>
2016-12-11 00:59:51 +0100

    Cosmetic

--- app/Main.hs
+++ app/Main.hs
@@ -19,45 +19,46 @@ defaultStack = "middleware"
 nixFile z = z <> ".nix"
 nixCommand z = Text.unwords ["nix-shell", nixFile z]
 
-runCommand :: Text -> Optional Text -> [Text] -> Shell ExitCode
-runCommand zone cmd cargs  =  do
+runCommand :: Text -> Optional Text -> [Text] -> IO ()
+runCommand zone cmd cargs  =  sh $ do
   let
     pgr = nixCommand zone <>  " -I nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/" <> nixpkgs <> ".tar.gz"
     cmdStr = Optional.optional pgr (\c -> pgr <> " --command '" <> Text.unwords (c : cargs) <> "'" ) cmd
 
   pushd projectDir
   initEnv zone
-  liftIO $ interactive cmdStr
+  interactive cmdStr
 
-initEnv :: Text -> Shell ExitCode
-initEnv zone = do
-  let nixfile = fromText (nixFile zone)
-  foundFile <- testfile nixfile
-  if foundFile
-    then pure ExitSuccess
-    else proc "./set_nix.sh" [zone] empty
+  where
+    initEnv :: Text -> Shell ExitCode
+    initEnv zone = do
+      let nixfile = fromText (nixFile zone)
+      foundFile <- testfile nixfile
+      if foundFile
+        then pure ExitSuccess
+        else proc "./set_nix.sh" [zone] empty
 
-run (Options zone Console) = sh (runCommand zone empty empty)
-run (Options zone Stats) = sh (runCommand zone "stats" empty)
-run (Options zone (Stack (StackData stack key))) = sh (runCommand zone "stack_data_for" (key : toList stack))
-run (Options zone (Stack (StackFacts stack Nothing ))) = sh (runCommand zone "stack_facts"  (toList stack))
-run (Options zone (Stack (StackFacts stack (Just target)))) = sh (runCommand zone "stack_facts_on" (target: toList stack))
-run (Options zone (Stack (StackOrchestrate stack cmd))) = sh (runCommand zone "stack_orch" (cmd : toList stack))
-run (Options zone (Stack (StackPing stack))) = sh (runCommand zone "stack_ping" (toList stack))
-run (Options zone (Stack (StackRunPuppet stack target ))) = sh (runCommand zone "stack_runpuppet_on" (target : toList stack))
-run (Options zone (Stack (StackSync stack))) = sh (runCommand zone "stack_sync" (toList stack))
-run (Options zone (Node (NodeFacts node))) = sh (runCommand zone "node_facts" [node])
-run (Options zone (Node (NodeData node))) = sh (runCommand zone "node_data" [node])
-run (Options zone (Node (NodeDu node))) = sh (runCommand zone "node_du" [node])
-run (Options zone (Node (NodeRunPuppet node))) = sh (runCommand zone "node_runpuppet" [node])
-run (Options zone (Result (ResultNum n))) = sh (runCommand zone "result" [show n])
-run (Options zone (Result (ResultJob n ))) = sh (runCommand zone "result_for" [n])
+run (Options zone Console)                                  = runCommand zone empty empty
+run (Options zone Stats)                                    = runCommand zone "stats" empty
+run (Options zone (Stack (StackData stack key)))            = runCommand zone "stack_data_for" (key : toList stack)
+run (Options zone (Stack (StackFacts stack Nothing )))      = runCommand zone "stack_facts"  (toList stack)
+run (Options zone (Stack (StackFacts stack (Just target)))) = runCommand zone "stack_facts_on" (target: toList stack)
+run (Options zone (Stack (StackOrchestrate stack cmd)))     = runCommand zone "stack_orch" (cmd : toList stack)
+run (Options zone (Stack (StackPing stack)))                = runCommand zone "stack_ping" (toList stack)
+run (Options zone (Stack (StackRunPuppet stack target )))   = runCommand zone "stack_runpuppet_on" (target : toList stack)
+run (Options zone (Stack (StackSync stack)))                = runCommand zone "stack_sync" (toList stack)
+run (Options zone (Node (NodeFacts node)))                  = runCommand zone "node_facts" [node]
+run (Options zone (Node (NodeData node)))                   = runCommand zone "node_data" [node]
+run (Options zone (Node (NodeDu node)))                     = runCommand zone "node_du" [node]
+run (Options zone (Node (NodeRunPuppet node)))              = runCommand zone "node_runpuppet" [node]
+run (Options zone (Result (ResultNum n)))                   = runCommand zone "result" [show n]
+run (Options zone (Result (ResultJob n )))                  = runCommand zone "result_for" [n]
 
 
 main :: IO ()
 main = options "CICD command line utility" parser >>= run
 
-interactive :: Text -> IO ExitCode
+interactive :: MonadIO io => Text -> io ExitCode
 interactive c = do
     let
       c' = Text.unpack c
@@ -67,5 +68,5 @@ interactive c = do
             , Process.std_err = Process.Inherit
             , Process.delegate_ctlc = True
             }
-    (_, _, _, ph) <- Process.createProcess cp
-    Process.waitForProcess ph
+    (_, _, _, ph) <- liftIO $ Process.createProcess cp
+    liftIO $ Process.waitForProcess ph
