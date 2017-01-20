018f9cfe1f45ea5d1869e632a6c8dee271942045
Pierre Radermecker <pradermecker@cirb.brussels>
2017-01-20 18:52:45 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,15 +1,16 @@
-{ mkDerivation, base, lens, optional-args, process, raw-strings-qq
-, stdenv, text, turtle
+{ mkDerivation, base, jq, lens, optional-args, pepper, process
+, raw-strings-qq, stdenv, text, turtle
 }:
 mkDerivation {
   pname = "cicd-shell";
-  version = "0.9.0";
+  version = "0.9.7";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     base lens optional-args process raw-strings-qq text turtle
   ];
+  executableSystemDepends = [ jq pepper ];
   homepage = "ssh://git@stash.cirb.lan:7999/cicd/salt-shell.git";
   license = stdenv.lib.licenses.bsd3;
 }
