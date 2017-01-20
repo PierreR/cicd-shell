a7299839cb504753f8bf2ca7fe932f3a9f36ab01
Pierre Radermecker <pradermecker@cirb.brussels>
2017-01-20 23:04:58 +0100

    Move pinning nixpkgs logic into share/default.nix

--- share/default.nix
+++ share/default.nix
@@ -1,8 +1,15 @@
 { salt-user, salt-pass, salt-url, zone  }:
-with import <nixpkgs> {};
+let
+  bootstrap = import <nixpkgs> { };
+in
+with import (bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs";
+    inherit (builtins.fromJSON (builtins.readFile ./.nixpkgs.json)) rev sha256;
+  }) { };
 stdenv.mkDerivation {
   name = "pepper-env";
-  buildInputs = [ pepper jq ];
+  buildInputs = [ pepper jq haskellPackages.language-puppet_1_3_4 ];
   shellHook = ''
   export SALTAPI_USER="${salt-user}"
   export SALTAPI_PASS="${salt-pass}"
@@ -11,5 +18,6 @@ stdenv.mkDerivation {
   export ZONE="${zone}"
   export PS1="\n\[\033[1;32m\][cicd ${zone}]$\[\033[0m\] "
   alias cicd="cicd $ZONE"
+  alias pep="pepper"
   '';
 }
