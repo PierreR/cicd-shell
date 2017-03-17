a8b0964680e8071954507004f3a795bdb19bdf96
Pierre Radermecker <pradermecker@cirb.brussels>
2017-03-17 23:36:56 +0100

    Use one single shell.nix for nix-build and nix-shell

--- shell.nix
+++ shell.nix
@@ -1,1 +1,20 @@
-(import ./release.nix).project.env
+# You can build this repository using Nix by running:
+#
+#     $ nix-build shell.nix
+#
+# You can also open up this repository inside a Nix shell by running:
+#
+#     $ nix-shell
+#
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+  inherit (nixpkgs) pkgs;
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+  drv = haskellPackages.callPackage ./. {dhall = haskellPackages.dhall_git;};
+
+in
+
+if pkgs.lib.inNixShell then drv.env else drv
