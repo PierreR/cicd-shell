c278cc7c1bdcd5ed609d7853e45fb5f28dccae6f
Pierre Radermecker <pradermecker@cirb.brussels>
2017-03-17 16:15:19 +0100

    Use overlays

--- release.nix
+++ release.nix
@@ -5,13 +5,6 @@ let
     packageOverrides = pkgs: rec {
       haskellPackages = pkgs.haskellPackages.override {
         overrides = self: super: rec {
-          dhall = self.callCabal2nix "dhall" (pkgs.fetchFromGitHub {
-            owner  = "Gabriel439";
-            repo   = "Haskell-Dhall-Library";
-            rev    = "11ceab1dfeb9ed9a25dab717b4fe24ffaf7d320e";
-            sha256 = "00iz2albmj3iw8sdj2idf1y4vgfjfliv7xcxbqgmb3ggp7n7wf6a";
-          }) {};
-          cicd-shell = self.callPackage ./. {inherit dhall;};
         };
       };
     };
@@ -25,12 +18,7 @@ let
 
   pkgs = import src { inherit config; };
 in
-rec {
-  project = pkgs.haskellPackages.cicd-shell;
-  cicd-shell = pkgs.buildEnv rec {
-    name = "cicd-shell-${project.version}";
-    paths = [
-      project
-    ];
-  };
+{
+  project = pkgs.haskellPackages.callPackage ./. {};
 }
+
