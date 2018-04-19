d7b53d79b9b9366eae2f2c6fdc1a52994e6b6072
Pierre Radermecker <pradermecker@cirb.brussels>
2018-04-19 17:35:41 +0200

    Replace foldl1 with foldr1

--- src/Shell/PepCmd/Utils.hs
+++ src/Shell/PepCmd/Utils.hs
@@ -18,7 +18,7 @@ pepperCompoundTarget across t
       let role_target (Role (Just (Subgroup g')) r') = "G@subgroup:" <> g' <> " and G@role:" <> r'
           role_target (Role Nothing r') = "G@role:" <> r'
           surround a b c = a <> c <> b
-          foldmap_hostgroup = surround "( " " )" . foldr1 (\a b -> a <> " or " <> b) . map ("G@hostgroup:"<>)
+          foldmap_hostgroup = surround "( " " )" . foldl1 (\a b -> a <> " or " <> b) . map ("G@hostgroup:"<>)
       in joinTargetWith " and "
            [ Just ("G@zone:" <> z)
            , foldmap_hostgroup <$> sx
