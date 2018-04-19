600caf0a0b68a337736fa87f85cb682bc1a918dc
Pierre Radermecker <pradermecker@cirb.brussels>
2018-04-20 01:14:59 +0200

    Automate golden file testing

--- tests/Shell/PepCmdTest.hs
+++ tests/Shell/PepCmdTest.hs
@@ -1,18 +1,23 @@
-{-# LANGUAGE OverloadedLists #-}
 module Shell.PepCmdTest where
 
 import           Shell.PepCmd.Utils
 import           Shell.Prelude
 import           Shell.Type
 
+import qualified Data.Text.Lazy          as Text
 import qualified Data.Text.Lazy.Encoding as Text
-import qualified Data.Text.Lazy as Text
+import qualified System.FilePath         as FilePath
 import           Test.Tasty
 import           Test.Tasty.Golden
 
 
-test_compountTarget :: TestTree
+test_compountTarget :: IO TestTree
 test_compountTarget = do
-  let Just t = readTarget "cicd.salt.master.dev"
-  goldenVsString "golden" "tests/Shell/cicd-salt-master.golden"
-    $ pure (Text.encodeUtf8 (Text.fromStrict (pepperCompoundTarget False t)))
+  golden_files <- findByExtension [".golden"] "tests/Shell"
+  pure $ testGroup "pepper compound target"
+   [ goldenVsString fname golden_file
+       $ pure (Text.encodeUtf8 (Text.fromStrict (pepperCompoundTarget False target )))
+   | golden_file <- golden_files
+   , let fname = FilePath.takeBaseName golden_file
+         Just target = readTarget (toS fname)
+   ]
