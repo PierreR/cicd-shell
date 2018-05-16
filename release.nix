3e756212777769ffac409ee440101af4f5d43c4f
Pierre Radermecker <pradermecker@cirb.brussels>
2018-05-17 00:19:22 +0200

    Add docker images deployment recipe

--- release.nix
+++ release.nix
@@ -1,7 +1,8 @@
 let
-  pinned = import ~/.config/nixpkgs/pin.nix;
-  hlib = pinned.haskell.lib;
-  haskellPackages = pinned.haskellPackages;
+  pkgs = import ~/.config/nixpkgs/pin.nix;
+  dockerTools = pkgs.dockerTools;
+  hlib = pkgs.haskell.lib;
+  haskellPackages = pkgs.haskellPackages;
 in
 rec {
   project = hlib.dontHaddock
@@ -11,17 +12,18 @@ rec {
       )
     );
 
-  # docker = pkgs.dockerTools.buildImage {
-  #   name = "pi3r/cicd-shell";
-  #   runAsRoot = ''
-  #     #!${pkgs.stdenv.shell}
-  #     mkdir /bin /tmp
-  #     ln -s ${pkgs.bash}/bin/sh /bin/sh
-  #   '';
-  #   # config.Entrypoint = [ "${self.bash}/bin/bash"];
-  #   config.Entrypoint = [ "${project}/bin/cicd"];
-  #   config.Env = [
-  #     "PATH=${pkgs.bash}/bin:${pkgs.iputils.out}/bin:${pkgs.jq}/bin:${pkgs.pinned}/bin:${pkgs.coreutils}/bin"
-  #   ];
-  # };
+  docker = dockerTools.buildImage {
+    name = "pi3r/cicd-shell";
+    fromImage = dockerTools.pullImage {
+      imageName = "nixos/nix";
+      imageDigest = "sha256:d414c533649e405f2a5966b5019f042c84e511cf468965c9ef03719ae2b0c124";
+      sha256 = "045swkdqjh24pgb8kb798ya34my5ryvbikzavxi3ns8y7l0prxff";
+      finalImageTag = "2.0";
+    };
+    config.Entrypoint = [ "${pkgs.bash}/bin/bash"];
+    # config.Entrypoint = [ "${project}/bin/cicd"];
+    config.Env = [
+      "PATH=/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin:${project}/bin:${pkgs.jq}/bin:${pkgs.pepper}/bin"
+    ];
+  };
 }
