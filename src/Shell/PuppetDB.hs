e79c79fc6101b02df031e57c42f9ca0248acaeb4
Pierre Radermecker <pradermecker@cirb.brussels>
2018-05-16 19:24:47 +0200

    Use v4 api in PuppetDB module

--- src/Shell/PuppetDB.hs
+++ src/Shell/PuppetDB.hs
@@ -10,13 +10,12 @@ import qualified Shell.Config       as Config
 import           Shell.Prelude
 import NeatInterpolation
 
-
 getFacts :: MonadIO m => Text -> m LByteString
 getFacts n = runReq def $ do
   let queryval = [text| ["=", "certname", "$n"] |] :: Text
       queryparam = "query" =: queryval
   r <- req GET
-    (http Config.puppetdbServer /: "v3" /: "facts")
+    (http Config.puppetdbServer /: "v4" /: "facts")
     NoReqBody
     lbsResponse
     $ (port Config.puppetdbPort)  <> queryparam
